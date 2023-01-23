7f5875e363946d1ca66b0123cd5a733e4803e2a7
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-01-23 13:20:53 +0100

    Add submit new event feedback (Loading animation)

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,12 +1,19 @@
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, Form, useNavigation } from 'react-router-dom';
+import React from 'react';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   const navigate = useNavigate();
   function cancelHandler() {
-    navigate({pathname: '..', hash: "#"});
+    navigate('..');
   }
+
+
+  // const loadingIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z" /></svg>
+
 
   return (
     <Form method='post' className={classes.form}>
@@ -30,7 +37,7 @@ function EventForm({ method, event }) {
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? <div className={classes.load}></div> : 'Save'}</button>
       </div>
     </Form>
   );
