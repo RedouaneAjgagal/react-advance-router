705ee88ca8c0c43d4abbde60f51efe96780b0893
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-01-29 18:55:56 +0100

    Add newsletter requirment of an email and error UI

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -3,6 +3,7 @@ import { useFetcher } from 'react-router-dom';
 import { useEffect } from 'react';
 
 function NewsletterSignup() {
+    let errorStyle = null
     const fetcher = useFetcher();
     const { data, state } = fetcher;
     useEffect(() => {
@@ -10,6 +11,10 @@ function NewsletterSignup() {
             window.alert(`${data.message} && ${data.email}`)
         }
     }, [data, state])
+    const invalidEmail = data?.errorMsg
+    if (invalidEmail) {
+        errorStyle = { backgroundColor: '#ff8d8d' }
+    }
     return (
         <fetcher.Form method="post" action='/newsletter' className={classes.newsletter}>
             <input
@@ -17,6 +22,7 @@ function NewsletterSignup() {
                 placeholder="Sign up for newsletter..."
                 aria-label="Sign up for newsletter"
                 name='email'
+                style={invalidEmail && errorStyle}
             />
             <button>Sign up</button>
         </fetcher.Form>
--- frontend/src/pages/Newsletter.js
+++ frontend/src/pages/Newsletter.js
@@ -4,17 +4,19 @@ import NewsletterSignup from '../components/NewsletterSignup';
 const Newsletter = () => {
   return (
     <div>
-        <h1>Join Our Newsletter!</h1>
-        <NewsletterSignup />
+      <h1>Join Our Newsletter!</h1>
+      <NewsletterSignup />
     </div>
   )
 }
 
 export default Newsletter
 
-export const action = async ( {request} ) => {
-    const response = await request.formData();
-    const email = response.get('email');
-    // send data to the back end
-    return {message: 'signup successfull!', email}
+export const action = async ({ request }) => {
+  const response = await request.formData();
+  const email = response.get('email');
+  const validEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  if (!validEmail.test(email)) return {errorMsg: 'Invalid Email'}
+  // send data to the back end
+  return { message: 'signup successfull!', email }
 }
